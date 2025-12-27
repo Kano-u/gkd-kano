d36a9c2d4340f3049704ad5d4a3d82d455dc8179
Kano <3346740146@qq.com>
2025-12-27 13:15:51 +0800

    new(支付宝)：点击"确认不选择"

---
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -0,0 +1,19 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.eg.android.AlipayGphone',
+  name: '支付宝',
+  groups: [
+    {
+      key: 1, // 规则组的唯一ID，自己定一个数字即可
+      name: '关闭特定弹窗', // 给这个规则起个名字，方便自己认
+      desc: '自动点击"确认不选择"', // 描述
+      rules: [
+        {
+          matches: '[text="确认不选择"]', // 核心：匹配文字内容
+          snapshotUrls: '', // 如果有快照链接可以填这里，没有留空
+        },
+      ],
+    },
+  ],
+});
